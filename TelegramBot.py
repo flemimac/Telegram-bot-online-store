5d17718bd0196621395469062686d0da9e8d266a
Alexander <alex.miazim.2006@gmail.com>
2023-05-20 18:35:41 +0200

    edit database, Telegrambot
    add Laminate in database and script

--- TelegramBot.py
+++ TelegramBot.py
@@ -8,13 +8,17 @@ bot = telebot.TeleBot('6014768020:AAFXQbD37VvsqVBO0wVy83YCHOKzDkGoOrE')
 
 # Фотографии
 # Клей  
-photo_glue1kg = open('C:\BotTelegramPython\database\PVA\PVA3KG.jpg', 'rb')
-photo_glue750g = open('C:\BotTelegramPython\database\PVA\PVA750G.jpg', 'rb')
-photo_glue250g = open('C:\BotTelegramPython\database\PVA\PVA250G.jpg', 'rb')
+photo_glue1kg = open(r'C:\Users\CODOLOGIA\Desktop\фыв\GitHub\Telegram-bot-online-store\database\PVA\PVA3KG.jpg', 'rb')
+photo_glue750g = open(r'C:\Users\CODOLOGIA\Desktop\фыв\GitHub\Telegram-bot-online-store\database\PVA\PVA750G.jpg', 'rb')
+photo_glue250g = open(r'C:\Users\CODOLOGIA\Desktop\фыв\GitHub\Telegram-bot-online-store\database\PVA\PVA250G.jpg', 'rb')
 
 # Краска
-photo_paint1L = open('C:\BotTelegramPython\database\Paint\PaintDulux1L.jpg', 'rb')
-photo_paint25L = open('C:\BotTelegramPython\database\Paint\PaintDulux25L.jpg', 'rb')
+photo_paint1L = open(r'C:\Users\CODOLOGIA\Desktop\фыв\GitHub\Telegram-bot-online-store\database\Paint\PaintDulux1L.jpg', 'rb')
+photo_paint25L = open(r'C:\Users\CODOLOGIA\Desktop\фыв\GitHub\Telegram-bot-online-store\database\Paint\PaintDulux25L.jpg', 'rb')
+
+# Ламинат
+laminate_hudson = open(r'C:\Users\CODOLOGIA\Desktop\фыв\GitHub\Telegram-bot-online-store\database\Laminate\LaminateOakHudson.jpg', 'rb')
+laminate_petra = open(r'C:\Users\CODOLOGIA\Desktop\фыв\GitHub\Telegram-bot-online-store\database\Laminate\LaminateOakPetra.jpg', 'rb')
 
 # Старт бота
 @bot.message_handler(commands=['start'])
@@ -25,7 +29,6 @@ def start_message(message):
     markup.add(btn_store, btn_question)
     bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я тестовый бот для магазина СтройБаза".format(message.from_user), reply_markup=markup)
 
-
 @bot.message_handler(content_types=['text'])
 def func(message):
     a = str(message) # Получаем из ответа пользователя "message" и придаем ему аргумент "a"
@@ -35,9 +38,10 @@ def func(message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn_glue = types.KeyboardButton("Клей") 
         btn_paint = types.KeyboardButton("Краска")
+        btn_laminate = types.KeyboardButton("Ламинат")
         btn_back = types.KeyboardButton("Назад")
-        markup.add(btn_glue, btn_paint, btn_back)
-        bot.send_message(message.chat.id, text='В ассортименте имеется:\n 1. Клей ПВА Момент столяр\n 2. Краска для пола Dulux', reply_markup=markup)
+        markup.add(btn_glue, btn_paint, btn_laminate, btn_back)
+        bot.send_message(message.chat.id, text='В ассортименте имеется:\n1. Клей ПВА Момент столяр\n2. Краска для пола Dulux\n 3. Ламинат "Дуб Хадсон"', reply_markup=markup)
     
     # Клей
     elif message.text == "Клей":
@@ -63,6 +67,7 @@ def func(message):
                 markup.add(btn_250g, btn_750g, btn_back)
                 bot.send_message(message.chat.id, text='Товара нет', reply_markup=markup)
             else: # Если да - показывает описание и фото
+
                 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
                 btn_750g = types.KeyboardButton("Клей 750 грамм")
                 btn_250g = types.KeyboardButton("Клей 250 грамм ")
@@ -162,6 +167,52 @@ def func(message):
                 markup.add(btn_1l, btn_back)
                 bot.send_message(message.chat.id, text='Краска для пола Dulux 2,5 литров', reply_markup=markup)
                 bot.send_photo(message.chat.id, photo_paint25L)
+    
+    elif message.text == "Ламинат":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn_oakpetra = types.KeyboardButton('Ламинат "Дуб Петра"')
+        btn_oakhudson = types.KeyboardButton('Ламинат "Дуб Хадсон"')
+        btn_back = types.KeyboardButton("Назад")
+        markup.add(btn_oakhudson, btn_oakpetra, btn_back)
+        bot.send_message(message.chat.id, text='Какой хотите ламинат?', reply_markup=markup)
+
+    elif message.text == 'Ламинат "Дуб Хадсон"':
+        with con:
+            data = con.execute("SELECT * FROM database WHERE name=?", [a])
+            res = data.fetchone()
+
+            if res:
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                btn_oakpetra = types.KeyboardButton('Ламинат "Дуб Петра"')
+                btn_back = types.KeyboardButton("Назад")
+                markup.add(btn_oakpetra, btn_back)
+                bot.send_message(message.chat.id, text='Товара нет', reply_markup=markup)
+            else:
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                btn_oakpetra = types.KeyboardButton('Ламинат "Дуб Петра"')
+                btn_back = types.KeyboardButton("Назад")
+                markup.add(btn_oakpetra, btn_back)
+                bot.send_message(message.chat.id, text='Ламинат "Дуб Хадсон"', reply_markup=markup)
+                bot.send_photo(message.chat.id, laminate_hudson)
+
+    elif message.text == 'Ламинат "Дуб Петра"':
+        with con:
+            data = con.execute("SELECT * FROM database WHERE name=?", [a])
+            res = data.fetchone()
+
+            if res:
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                btn_oakhudson = types.KeyboardButton('Ламинат "Дуб Хадсон"')
+                btn_back = types.KeyboardButton("Назад")
+                markup.add(btn_oakhudson, btn_back)
+                bot.send_message(message.chat.id, text='Товара нет', reply_markup=markup)
+            else:
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                btn_oakhudson = types.KeyboardButton('Ламинат "Дуб Хадсон"')
+                btn_back = types.KeyboardButton("Назад")
+                markup.add(btn_oakhudson, btn_back)
+                bot.send_message(message.chat.id, text='Ламинат "Дуб Петра"', reply_markup=markup)
+                bot.send_photo(message.chat.id, laminate_petra)
 
 
     # Возврат в главное меню
